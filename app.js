af0f7286363fa84c8d0f9819389bd643747b49b7
saisuniltata <saisuniltata@gmail.com>
2017-02-15 18:07:23 -0600

    changed home

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require("path");
 app.use(express.static(__dirname + '/styles'));
 app.use(express.static(__dirname + '/assets'));
-app.get('/community', function (req, res) {
+app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname + '/Community-support-full.html'));
 });
 app.get('/payment', function (req, res) {
