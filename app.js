3447753746baa41793189936c4395b1da08b87fc
saisuniltata <saisuniltata@gmail.com>
2017-02-15 18:28:08 -0600

    changed port

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app = express();
 var path = require("path");
-app.use(express.static(__dirname + '/styles'));
-app.use(express.static(__dirname + '/assets'));
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname + '/Community-support-full.html'));
+var port = process.env.PORT || 3000;
+app.use('/', express.static(__dirname));
+app.use('/css', express.static(__dirname + '/styles'));
+app.use('/assets', express.static(__dirname + '/assets'));
+app.get('/*', function (req, res) {
+	res.sendFile('Community-support-full.html');
 });
 app.get('/payment', function (req, res) {
 	res.sendFile(path.join(__dirname + '/payment-portal-full.html'));
 });
-app.listen(3000);
+app.listen(port);
 console.log("listening on port 3000");
